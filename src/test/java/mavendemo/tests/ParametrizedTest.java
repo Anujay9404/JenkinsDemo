457ea9bf20f15e924da69b056d66a062cf26671f
Anujaya V <anujayav1997@gmail.com>
2021-10-19 00:37:45 +0530

    edit in testing files

--- src/test/java/mavendemo/tests/ParametrizedTest.java
+++ src/test/java/mavendemo/tests/ParametrizedTest.java
@@ -1,37 +1,37 @@
-package mavendemo.tests;
+// package mavendemo.tests;
 
-import static org.junit.Assert.*;
+// import static org.junit.Assert.*;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-
-
-import mavendemo.util.MathUtils;
+// import org.junit.Assert;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.junit.runners.Parameterized;
+// import org.junit.runners.Parameterized.Parameters;
 
 
-@RunWith(Parameterized.class)
-public class ParametrizedTest {
-	@Parameters(name = "Run #Square of : {0}^2={1}")
-	public static Iterable<Object [] > data(){
-		return Arrays.asList(new Object [][]{ {1,1}, {2,4}, {3,9}, {4,16}, {5,25} });
-	}
+// import mavendemo.util.MathUtils;
+
+
+// @RunWith(Parameterized.class)
+// public class ParametrizedTest {
+// 	@Parameters(name = "Run #Square of : {0}^2={1}")
+// 	public static Iterable<Object [] > data(){
+// 		return Arrays.asList(new Object [][]{ {1,1}, {2,4}, {3,9}, {4,16}, {5,25} });
+// 	}
 	
-	private final int input;
-	private final int resultExpected;
+// 	private final int input;
+// 	private final int resultExpected;
 	
-	public ParametrizedTest(final int input, final int result){
-		this.input = input;
-		this.resultExpected = result;
-	}
+// 	public ParametrizedTest(final int input, final int result){
+// 		this.input = input;
+// 		this.resultExpected = result;
+// 	}
 	
-	@Test
-	public void testUserMapping(){
-		Assert.assertEquals(resultExpected, MathUtils.square(input));
-	}
+// 	@Test
+// 	public void testUserMapping(){
+// 		Assert.assertEquals(resultExpected, MathUtils.square(input));
+// 	}
 
-}
+// }
